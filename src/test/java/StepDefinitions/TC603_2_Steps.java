df88ba3e3f640fe58e3bf7916ced74bd72633467
SerkanSeweoster <156962646+SerkanSeweoster@users.noreply.github.com>
2025-05-10 00:22:08 +0200

    TC604

--- src/test/java/StepDefinitions/TC603_2_Steps.java
+++ src/test/java/StepDefinitions/TC603_2_Steps.java
@@ -10,11 +10,18 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class TC603_2_Steps {
     LeftNav ln = new LeftNav(GWD.getDriver());
     DialogContent dc = new DialogContent(GWD.getDriver());
-    String accountNumber="";
+    String accountNumber = "";
+    String payingAmount = "";
 
     @When("the user navigates to the Bill Pay page")
     public void theUserNavigatesToTheBillPayPage() {
@@ -23,16 +30,16 @@ public class TC603_2_Steps {
 
     @And("the user fills in the water bill form with valid random data")
     public void theUserFillsInTheWaterBillFormWithValidRandomData() {
-        dc.mySendKeys(dc.payeName,"İski Su Faturasi");
+        dc.mySendKeys(dc.payeName, "İski Su Faturasi");
         dc.mySendKeys(dc.payeAddressStreet, FakerUtil.getAddress());
-        dc.mySendKeys(dc.payeAddressCity,FakerUtil.getCity());
-        dc.mySendKeys(dc.payeAddressState,FakerUtil.getState());
-        dc.mySendKeys(dc.payeAddressZipCode,FakerUtil.getZipCode());
-        dc.mySendKeys(dc.payePhoneNumber,FakerUtil.getPhoneNumber());
-        dc.mySendKeys(dc.payeAccountNumber,"12345");
-        dc.mySendKeys(dc.verifyAccount,"12345");
-        dc.mySendKeys(dc.amountOdenecekTutar,"1");
-        accountNumber =dc.senderAccountSelector.getText();
+        dc.mySendKeys(dc.payeAddressCity, FakerUtil.getCity());
+        dc.mySendKeys(dc.payeAddressState, FakerUtil.getState());
+        dc.mySendKeys(dc.payeAddressZipCode, FakerUtil.getZipCode());
+        dc.mySendKeys(dc.payePhoneNumber, FakerUtil.getPhoneNumber());
+        dc.mySendKeys(dc.payeAccountNumber, "12345");
+        dc.mySendKeys(dc.verifyAccount, "12345");
+        dc.mySendKeys(dc.amountOdenecekTutar, "45");
+        accountNumber = dc.senderAccountSelector.getText();
     }
 
     @And("the user submits the payment")
@@ -44,8 +51,18 @@ public class TC603_2_Steps {
     public void thePaymentConfirmationShouldDisplayTheCorrectAmountAndAccountNumber() {
         ln.myClick(ln.lnAccountsOverview);
         dc.myClick(dc.Account);
+        WebElement a = dc.wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='transactionTable']//a")));
+        a.click();
 
+        dc.wait.until(ExpectedConditions.visibilityOf(dc.transactionDate));
+        String actualDate = dc.transactionDate.getText();
 
+        String expectedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+        Assert.assertEquals(actualDate, expectedDate, "Tarih beklenenden farklı!");
+
+        Assert.assertTrue(dc.transactionDescription.getText().contains("Bill Payment to İski Su Faturasi"));
+
+        Assert.assertTrue(dc.transactionAmount.getText().contains("$45"));
 
     }
 }
